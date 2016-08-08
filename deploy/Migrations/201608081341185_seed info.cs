bdf495fa4a17cc56c7d144f2263ff187e4770980
Olivia Taylor <obt1@pitt.edu>
2016-08-08 13:23:25 -0400

    working project with some css

---
+++ deploy/Migrations/201608081341185_seed info.cs
@@ -0,0 +1,16 @@
+namespace deploy.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class seedinfo : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
